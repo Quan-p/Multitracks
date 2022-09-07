4671a8d1504e0e9c15209c4b57aaf3593c1d0625
quan-p <m.phamquan@gmail.com>
2022-09-07 16:27:01 -0500

    Import react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './App.scss';
+import './Header/Header';
+import Header from './Header/Header';
 
 function App() {
   return (
     <div className="App">
-        <>Test</>
+        <Header>
+        </Header>
     </div>
   );
 }
