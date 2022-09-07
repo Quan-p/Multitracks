594e7c15283a6ab7ad4a78d8f354f636c412912a
quan-p <m.phamquan@gmail.com>
2022-09-07 18:04:01 -0500

    Create footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import './Header/Header';
 import Header from './Header/Header';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
     <div className="App">
         <Header>
         </Header>
+        <Footer />
     </div>
   );
 }
---
+++ src/Footer/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Footer.scss";
+
+function Footer() {
+    return (
+        <div className="footer-container">
+            <div className="footer-primary">
+                <div className="footer-grid">
+                    <div className="footer-column-one">
+                        <h2>Products</h2>
+                    </div>
+                    <div className="footer-column-two">
+                        <h2>Resources</h2>
+                        <h2>Company</h2>
+                    </div>
+                    <div className="footer-column-three">
+                        <h2>Store</h2>
+                        <h2>Extras</h2>
+                    </div>
+                    <div className="footer-column-three">
+                        <h2>Account</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="footer-secondary">
+
+            </div>
+        </div>
+            
+    )
+}
+
+export default Footer;
