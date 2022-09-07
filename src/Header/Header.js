f76d47c5c5a88562b260125c6f75b4b5951c2bf2
quan-p <m.phamquan@gmail.com>
2022-09-07 17:23:25 -0500

    Add all navbar links and lists

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -9,9 +9,49 @@ function Header() {
                     <a href='https://www.multitracks.com/resources/'>
                         <img src='https://mtracks.azureedge.net/public/images/site/logo/en/logo-mono.svg' alt='multi-tracks logo'/>
                     </a>
+                    <ul className='nav-links'>
+                        <li className='nav-list-items'>
+                            Products
+                        </li>
+                        <li className='nav-list-items'>
+                            <a href='https://www.multitracks.com/discovery/'>
+                                Songs
+                            </a>
+                        </li>
+                        <li className='nav-list-items'>
+                            <a href='https://www.multitracks.com/products/sounds/'>
+                                Sounds
+                            </a>
+                        </li>
+                        <li className='nav-list-items'>
+                            <a href='https://www.multitracks.com/pricing/'>
+                                Pricing
+                            </a>
+                        </li>
+                        <li className='nav-list-items'>
+                            More
+                        </li>
+                    </ul>
                 </div>
                 <div className='header-right'>
-                    
+                    <ul className='nav-links'>
+                        <li className='nav-list-items'>
+                            <a href='https://www.multitracks.com/register/'>
+                                Account
+                            </a>
+                        </li>
+                        <li className='nav-list-items'>
+                            <a href='https://www.multitracks.com/cart/'>
+                                Cart
+                            </a>
+                        </li>
+                        <li className='nav-list-items'>
+                            Support
+                        </li>
+                        <li className='nav-list-items'>
+                            Search Icon
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
