b28d7b6d3382a5d13aa0e191916993f386da0624
quan-p <m.phamquan@gmail.com>
2022-09-09 17:59:58 -0500

    Create and style dropdown menu on hover

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -10,18 +10,51 @@ function Header() {
                         <img src='https://mtracks.azureedge.net/public/images/site/logo/en/logo-mono.svg' alt='multi-tracks logo'/>
                     </a>
                     <ul className='nav-links'>
-                        <li className='nav-list-items'>
+                        <li className='nav-list-items' id='product'>
                             Products
+                            <span>
+                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.37671 10.8276C8.21092 11.0575 7.83787 11.0575 7.63063 10.8276L5.76542 8.64368L4.10745 6.68966C3.85875 6.42146 4.066 6 4.48049 6H11.5268C11.8999 6 12.1486 6.42146 11.8999 6.68966L10.2419 8.64368L8.37671 10.8276Z" fill="#919191"/>
+                                </svg>
+                            </span>
+                            <div className='dropdown'>
+                                <ul className='dropdown-content'>
+                                    <li className='dropdown-link'>The Connected Stage</li>
+                                    <li className='dropdown-link'>Church Streaming License</li>
+                                    <li className='dropdown-link'>Tracks</li>
+                                    <li className='dropdown-link'>Playback</li>
+                                    <li className='dropdown-link'>Playback for Mac</li>
+                                    <li className='dropdown-link'>Pad Player</li>
+                                    <li className='dropdown-link'>Playback Rentals</li>
+                                    <li className='dropdown-link'>Cloud Pro</li>
+                                    <li className='dropdown-link'>RehearsalMix</li>
+                                    <li className='dropdown-link'>ChartBuilder</li>
+                                    <li className='dropdown-link'>Solo Practice Bundle</li>
+                                    <li className='dropdown-link'>Chart Pro</li>
+                                    <li className='dropdown-link'>ProPresenter Templates</li>
+                                    <li className='dropdown-link'>Sounds</li>
+                                </ul>
+                            </div>
                         </li>
                         <li className='nav-list-items'>
                             <a href='https://www.multitracks.com/discovery/'>
                                 Songs
                             </a>
+                            <span>
+                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.37671 10.8276C8.21092 11.0575 7.83787 11.0575 7.63063 10.8276L5.76542 8.64368L4.10745 6.68966C3.85875 6.42146 4.066 6 4.48049 6H11.5268C11.8999 6 12.1486 6.42146 11.8999 6.68966L10.2419 8.64368L8.37671 10.8276Z" fill="#919191"/>
+                                </svg>
+                            </span>
                         </li>
                         <li className='nav-list-items'>
                             <a href='https://www.multitracks.com/products/sounds/'>
                                 Sounds
                             </a>
+                            <span>
+                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.37671 10.8276C8.21092 11.0575 7.83787 11.0575 7.63063 10.8276L5.76542 8.64368L4.10745 6.68966C3.85875 6.42146 4.066 6 4.48049 6H11.5268C11.8999 6 12.1486 6.42146 11.8999 6.68966L10.2419 8.64368L8.37671 10.8276Z" fill="#919191"/>
+                                </svg>
+                            </span>
                         </li>
                         <li className='nav-list-items'>
                             <a href='https://www.multitracks.com/pricing/'>
@@ -30,7 +63,13 @@ function Header() {
                         </li>
                         <li className='nav-list-items'>
                             More
+                            <span>
+                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.37671 10.8276C8.21092 11.0575 7.83787 11.0575 7.63063 10.8276L5.76542 8.64368L4.10745 6.68966C3.85875 6.42146 4.066 6 4.48049 6H11.5268C11.8999 6 12.1486 6.42146 11.8999 6.68966L10.2419 8.64368L8.37671 10.8276Z" fill="#919191"/>
+                                </svg>
+                            </span>
                         </li>
+                        
                     </ul>
                 </div>
                 <div className='header-right'>
