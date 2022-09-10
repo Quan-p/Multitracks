327b205e52e04119a0f003f31e1ce82c48cd0a0e
quan-p <m.phamquan@gmail.com>
2022-09-10 18:11:33 -0500

    Style sidebar

--- src/Cart-sidebar/Sidebar.js
+++ src/Cart-sidebar/Sidebar.js
@@ -7,10 +7,31 @@ function Sidebar() {
             <div className="order-header">
                 <h3>Order Summary</h3>
             </div>
-            <div className="credits">
+            <div className="order-details credits" id="account">
                 <div>
                     Credits in your account
                 </div>
+                <span className="credit-val">
+                    234
+                </span>
+            </div>
+            <div className="order-details credits">
+                <div>
+                    Credits required
+                </div>
+                <span className="credit-val">
+                    234
+                </span>
+            </div>
+            <div className="order-details promo">
+                <div>
+                    Promo
+                </div>
+                <span>
+                    <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M4.99999 4.06889L0.958937 0L-1.14441e-05 0.965555L4.99999 6L9.99999 0.965555L9.04104 0L4.99999 4.06889Z" fill="#282828"/>
+                    </svg>
+                </span>
             </div>
         </div>
     )
