1e8d670b3388e1d3866dcfed5f0757e32ad2f26e
quan-p <m.phamquan@gmail.com>
2022-09-11 14:51:57 -0500

    Add hyperlinks to terms and condition and edit various needed colors

--- src/Cart-sidebar/Sidebar.js
+++ src/Cart-sidebar/Sidebar.js
@@ -28,8 +28,8 @@ function Sidebar() {
                     Promo
                 </div>
                 <span>
-                    <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M4.99999 4.06889L0.958937 0L-1.14441e-05 0.965555L4.99999 6L9.99999 0.965555L9.04104 0L4.99999 4.06889Z" fill="#282828"/>
+                    <svg width="10" height="6" viewBox="0 0 10 6" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M4.99999 4.06889L0.958937 0L-1.14441e-05 0.965555L4.99999 6L9.99999 0.965555L9.04104 0L4.99999 4.06889Z" fill="black"/>
                     </svg>
                 </span>
             </div>
@@ -71,7 +71,7 @@ function Sidebar() {
                     </span>
                 </div>
                 <span className="num-val">
-                    Change
+                    <a href="https://www.multitracks.com/" className="payment-change">Change</a>
                 </span>
             </div>
             <div className="order-details disclaimer">
@@ -86,7 +86,7 @@ function Sidebar() {
             </div>
             
             <div className="order-terms">
-                By placing your order, you agree to the Terms of Use and Privacy Policy.
+                By placing your order, you agree to the&nbsp;<a href="https://www.multitracks.com/terms/">Terms of Use</a>&nbsp;and&nbsp;<a href="https://www.multitracks.com/privacy/">Privacy Policy</a>.
             </div>
         </div>
     )
