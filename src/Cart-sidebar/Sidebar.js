51b1be96d5d52eae80b15ae7ef88f48cc977ca8a
quan-p <m.phamquan@gmail.com>
2022-09-10 19:32:01 -0500

    Add all content to sidebar

--- src/Cart-sidebar/Sidebar.js
+++ src/Cart-sidebar/Sidebar.js
@@ -11,7 +11,7 @@ function Sidebar() {
                 <div>
                     Credits in your account
                 </div>
-                <span className="credit-val">
+                <span className="num-val">
                     234
                 </span>
             </div>
@@ -19,7 +19,7 @@ function Sidebar() {
                 <div>
                     Credits required
                 </div>
-                <span className="credit-val">
+                <span className="num-val">
                     234
                 </span>
             </div>
@@ -32,6 +32,58 @@ function Sidebar() {
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M4.99999 4.06889L0.958937 0L-1.14441e-05 0.965555L4.99999 6L9.99999 0.965555L9.04104 0L4.99999 4.06889Z" fill="#282828"/>
                     </svg>
                 </span>
+            </div>
+            <div className="order-details subtotal">
+                <div>
+                    Subtotal
+                </div>
+                <span className="num-val">
+                    41
+                </span>
+            </div>
+            <div className="order-details tax">
+                <div>
+                    Tax
+                </div>
+                <span className="num-val">
+                    -
+                </span>
+            </div>
+            <div className="order-details total">
+                <div>
+                    Total
+                </div>
+                <span className="num-val total-val">
+                    41
+                </span>
+            </div>
+            <div className="order-details subtotal">
+                <div>
+                    Payment Method
+                </div>
+            </div>
+            <div className="order-details payment">
+                <div>
+                    American Express
+                    <span> </span>
+                    <span className="preview">
+                     - 1234
+                    </span>
+                </div>
+                <span className="num-val">
+                    Change
+                </span>
+            </div>
+            <div className="order-details disclaimer">
+                <div>
+                    All sales are final.
+                </div>
+            </div>
+            <button className="place-order-btn">
+                Place Order
+            </button>
+            <div className="order-terms">
+                By placing your order, you agree to the Terms of Use and Privacy Policy.
             </div>
         </div>
     )
